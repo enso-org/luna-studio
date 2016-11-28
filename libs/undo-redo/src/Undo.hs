f022bd0901cb7fd00ca0a1ee7cc5ed71f7f2888d
Sylwia <s.brodacka@gmail.com>
2016-11-28 10:39:58 +0100

    collectMessage refactor

--- libs/undo-redo/src/Undo.hs
+++ libs/undo-redo/src/Undo.hs
@@ -1,40 +1,53 @@
-module Undo.Undo where
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE ConstraintKinds           #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE TypeFamilies #-}
 
+module Undo where
 
+import Control.Monad.State
+import Control.Lens
+import Control.Exception.Base
+import           Data.ByteString                   (ByteString)
+import Data.Binary (Binary)
+import Data.Maybe
+import Prelude
 import qualified ZMQ.Bus.Bus                       as Bus
 import qualified ZMQ.Bus.Data.Message              as Message
 import           ZMQ.Bus.Data.MessageFrame         (MessageFrame (MessageFrame))
-import           ZMQ.Bus.Data.Topic                (Topic)
 import           ZMQ.Bus.EndPoint                  (BusEndPoints)
-import           ZMQ.Bus.Trans                     (BusT (..))
 import qualified ZMQ.Bus.Trans                     as Bus
 
 
 
-data UndoItem =  UndoItem { _event :: Maybe Request } deriving (Show, Eq, Generic) --cos innego tutaj niz request ale jeszcze ie weim co
+data UndoItem =  UndoItem { _userId  :: String
+                          , _content :: ByteString
+                           } deriving (Show, Eq)
 
-newtype UndoList = UndoList { _items :: [UndoItem] } deriving (Show, Eq, Generic)
+newtype UndoList = UndoList { _items :: [UndoItem] } deriving (Show, Eq)
 
+type Undo a = forall m. (MonadIO m, MonadState UndoList m) =>  m a
 
+makeLenses ''UndoList
+makeLenses ''UndoItem
 
-collect :: BusEndPoints -> [Topic] -> UndoList
-collect endPoints topics = Bus.runBus endPoints $ do
-    mapM_ Bus.subscribe topics
-    Bus.runBusT $ evalStateT $ forever collectEvents def
+empty :: UndoList
+empty = UndoList []
 
-collectEvents :: UndoList
-collectEvents = do
-    msgFrame <- lift $ BusT Bus.receive'
-    case msgFrame of
-        Left err -> Nothing
+runUndo :: BusEndPoints -> IO ()
+runUndo endPoints = evalStateT (collectEvents endPoints) empty
+
+collectEvents :: BusEndPoints -> Undo ()
+collectEvents endPoints = do
+    msgFrame <- Bus.runBus endPoints $ Bus.receive'
+    case (join msgFrame) of
         Right (MessageFrame msg corId senderId lastFrm) -> do
             let topic = msg ^. Message.topic
                 userId = show senderId
                 content = msg ^. Message.message
-            case Utils.lastPart '.' topic of
-                "request"  -> collectedMessage userId topic content
-                "response" -> collectedMessage userId topic content
-                "update"   -> collectedMessage userId topic content
+            collectedMessage userId content
 
-collectedMessage :: String -> String -> ByteString -> BusT ()
-collectedMessage userId topic content = do
+collectedMessage :: String -> ByteString -> Undo ()
+collectedMessage userId content = items %= (UndoItem userId content :)
