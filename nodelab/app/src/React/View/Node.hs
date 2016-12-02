b9297095de9bf1f421700b99815db02dcef716ca
Jan Kłosowski <janklosowski@gmail.com>
2016-12-02 11:06:06 +0100

    Node transform attribute

--- nodelab/app/src/React/View/Node.hs
+++ nodelab/app/src/React/View/Node.hs
@@ -15,45 +15,45 @@ import qualified React.Store.Node  as Node
 name :: JSString
 name = "node-editor"
 
-
+--FIXME: move all styles to CSS
 node :: Ref Node -> ReactView ()
 node nodeRef = React.defineControllerView
     name nodeRef $ \nodeStore () -> do
-
         let n = nodeStore ^. dt
         g_
             [ onClick       $ \_ _ -> Store.dispatch nodeRef $ UI.NodeEvent Node.OnClick
             , onDoubleClick $ \_ _ -> Store.dispatch nodeRef $ UI.NodeEvent $ Node.Enter $ n ^. Node.nodeId
             , "className" $= "node"
             , "viewbox"   $= "0 0 40 40"
+            , "transform" $= "translate(200 100)"
             ] $ do
-              circle_
-                  [ "className"   $= "self"
-                  , "cx"          $= "20"
-                  , "cy"          $= "20"
-                  , "r"           $= "5"
-                  , "fill"        $= "#8ABEB7"
-                  ] $ mempty
-              circle_
-                  [ "className"   $= "input"
-                  , "cx"          $= "20"
-                  , "cy"          $= "20"
-                  , "r"           $= "18.5"
-                  , "strokeWidth" $= "3px"
-                  , "stroke"      $= "#8ABEB7"
-                  , "fill"        $= "none"
-                  , "clipPath"    $= "url(#clipInput)"
-                  ] $ mempty
-              circle_
-                  [ "className"   $= "output"
-                  , "cx"          $= "20"
-                  , "cy"          $= "20"
-                  , "r"           $= "18.5"
-                  , "strokeWidth" $= "3px"
-                  , "stroke"      $= "#B294BB"
-                  , "fill"        $= "none"
-                  , "clipPath"    $= "url(#clipOutput)"
-                  ] $ mempty
+                circle_
+                    [ "className"   $= "self"
+                    , "cx"          $= "20"
+                    , "cy"          $= "20"
+                    , "r"           $= "5"
+                    , "fill"        $= "#8ABEB7"
+                    ] $ mempty
+                circle_
+                    [ "className"   $= "input"
+                    , "cx"          $= "20"
+                    , "cy"          $= "20"
+                    , "r"           $= "18.5"
+                    , "strokeWidth" $= "3px"
+                    , "stroke"      $= "#8ABEB7"
+                    , "fill"        $= "none"
+                    , "clipPath"    $= "url(#clipInput)"
+                    ] $ mempty
+                circle_
+                    [ "className"   $= "output"
+                    , "cx"          $= "20"
+                    , "cy"          $= "20"
+                    , "r"           $= "18.5"
+                    , "strokeWidth" $= "3px"
+                    , "stroke"      $= "#B294BB"
+                    , "fill"        $= "none"
+                    , "clipPath"    $= "url(#clipOutput)"
+                    ] $ mempty
 
 
 node_ :: Ref Node -> ReactElementM ViewEventHandler ()
