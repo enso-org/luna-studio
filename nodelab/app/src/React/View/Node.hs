d0ddf4b294943768fc472e7023c228374317ae2b
Jan Kłosowski <janklosowski@gmail.com>
2016-12-06 10:08:38 +0100

    Selection fix.

--- nodelab/app/src/React/View/Node.hs
+++ nodelab/app/src/React/View/Node.hs
@@ -35,23 +35,23 @@ node nodeRef = React.defineControllerView
                     [ "className" $= "selection-mark"
                     ] $ mempty
                 circle_
-                    [ "className"   $= "self"
+                    [ "className"   $= "port port--self"
                     , "fill"        $= "#8ABEB7"
                     ] $ mempty
                 path_
-                    [ "className"   $= "input input--01"
+                    [ "className"   $= "port port--i port--i--01"
                     , "fill"        $= "#8ABEB7"
-                    , "d"           $= "M18 0 A 20 20 0 0 0 0 18 H3 A 17 17 0 0 1 18 3"
+                    , "d"           $= "M18 0 A 20 20 1 0 0 0 18 H3 A 17 17 0 0 1 18 3 V0.1"
                     ] $ mempty
                 path_
-                    [ "className"   $= "input input--02"
+                    [ "className"   $= "port port--i port--i--02"
                     , "fill"        $= "#B5BD68"
-                    , "d"           $= "M0 22 A 20 20 0 0 0 18 40 V37 A 17 17 0 0 1 3 22"
+                    , "d"           $= "M0 22 A 20 20 0 0 0 18 40 V37 A 17 17 0 0 1 3 22 H0.1"
                     ] $ mempty
                 path_
-                    [ "className"   $= "output output--01"
+                    [ "className"   $= "port port--o port--o--01"
                     , "fill"        $= "#B294BB"
-                    , "d"           $= "M22 0 A 20 20.1 0 0 1 22 40 V37 A 17 17.1 0 0 0 22 3"
+                    , "d"           $= "M22 0 A 20 20.1 0 0 1 22 40 V37 A 17 17.1 0 0 0 22 3 V0.1"
                     ] $ mempty
 
 
