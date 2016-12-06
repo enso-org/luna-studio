20222d29e31f31f18e32644e28cd4e0ba566a2b8
pmlodawski <pmlodawski@dell.local>
2016-12-06 11:14:47 +0100

    Add Port View

---
+++ nodelab/app/src/React/View/Port.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+module React.View.Port where
+
+import qualified Event.UI          as UI
+import           React.Flux
+import qualified React.Flux        as React
+import           React.Store       (Ref, dispatch)
+import           React.Store.Node   (Node)
+import qualified React.Store.Node   as Node
+import           Utils.PreludePlus
+import Object.Widget.Port (Port)
+import qualified Object.Widget.Port as Port
+
+
+name :: JSString
+name = "port"
+
+
+port :: Ref Node -> ReactView Port
+port ref = React.defineView name $ \port -> do
+        elemString $ "port"
+
+
+port_ :: Ref Node -> Port -> ReactElementM ViewEventHandler ()
+port_ ref p = React.view (port ref) p mempty
