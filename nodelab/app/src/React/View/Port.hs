45c005e1d26c09583e41cde129f0ae0cf5788b28
Jan Kłosowski <janklosowski@gmail.com>
2016-12-09 00:56:03 +0100

    port generating function – preparation

--- nodelab/app/src/React/View/Port.hs
+++ nodelab/app/src/React/View/Port.hs
@@ -18,7 +18,32 @@ name = "port"
 
 port :: Ref Node -> ReactView Port
 port ref = React.defineView name $ \port -> do
-        elemString $ "port"
+    circle_
+        [ "className" $= "selection-mark"
+        ] mempty
+    circle_
+        [ "className" $= "port port--self"
+        , "fill"      $= "#8ABEB7"
+        , "stroke"    $= "#8ABEB7"
+        ] mempty
+    path_
+        [ "className" $= "port port--i port--i--01"
+        , "fill"      $= "#8ABEB7"
+        , "stroke"    $= "#8ABEB7"
+        , "d"         $= "M18 0 A 20 20 1 0 0 0 18 H3 A 17 17 0 0 1 18 3 V0.1"
+        ] mempty
+    path_
+        [ "className" $= "port port--i port--i--02"
+        , "fill"      $= "#B5BD68"
+        , "stroke"    $= "#B5BD68"
+        , "d"         $= "M0 22 A 20 20 0 0 0 18 40 V37 A 17 17 0 0 1 3 22 H0.1"
+        ] mempty
+    path_
+        [ "className" $= "port port--o port--o--01"
+        , "fill"      $= "#B294BB"
+        , "stroke"    $= "#B294BB"
+        , "d"         $= "M22 0 A 20 20.1 0 0 1 22 40 V37 A 17 17.1 0 0 0 22 3 V0.1"
+        ] mempty
 
 
 port_ :: Ref Node -> Port -> ReactElementM ViewEventHandler ()
