3076a5be09711f3cdd071ef337f55096786b343d
Jan Kłosowski <janklosowski@gmail.com>
2016-12-12 04:40:47 +0100

    Drawing port geometry

--- nodelab/app/src/React/View/Node.hs
+++ nodelab/app/src/React/View/Node.hs
@@ -2,7 +2,6 @@
 module React.View.Node where
 
 import qualified Data.Text.Lazy    as Text
-
 import           React.Flux
 import qualified React.Flux        as React
 
@@ -13,7 +12,6 @@ import qualified React.Store.Node  as Node
 import           React.View.Port   (port_)
 import           Utils.PreludePlus
 import           Utils.Vector      (x, y)
-
 
 
 name :: JSString
@@ -41,37 +39,16 @@ node nodeRef = React.defineControllerView
 
                 forM_ (n ^. Node.ports) $ port_ nodeRef
 
-                path_
-                    [ "className" $= "port port--i port--i--01"
-                    , "fill"      $= "#8ABEB7"
-                    , "stroke"    $= "#8ABEB7"
-                    , "d"         $= "M18 0 A 20 20 1 0 0 0 18 H3 A 17 17 0 0 1 18 3 V0.1"
-                    ] mempty
-                path_
-                    [ "className" $= "port port--i port--i--02"
-                    , "fill"      $= "#B5BD68"
-                    , "stroke"    $= "#B5BD68"
-                    , "d"         $= "M0 22 A 20 20 0 0 0 18 40 V37 A 17 17 0 0 1 3 22 H0.1"
-                    ] mempty
-                path_
-                    [ "className" $= "port port--o port--o--01"
-                    , "fill"      $= "#B294BB"
-                    , "stroke"    $= "#B294BB"
-                    , "d"         $= "M22 0 A 20 20.1 0 0 1 22 40 V37 A 17 17.1 0 0 0 22 3 V0.1"
-                    ] mempty
-
                 text_
                     [ "className" $= "name"
-                    , "x"         $= "20" -- FIXME: half of the node width
+                    , "x"         $= "20"
                     , "y"         $= "-16"
                     ] $ elemString $ Text.unpack $ n ^. Node.expression
                 text_
                     [ "className" $= "name"
-                    , "x"         $= "20" -- FIXME: half of the node width
+                    , "x"         $= "20"
                     , "y"         $= "65"
                     ] $ elemString $ Text.unpack $ n ^. Node.value
-
-
 
 
 node_ :: Ref Node -> ReactElementM ViewEventHandler ()
--- nodelab/app/src/React/View/Port.hs
+++ nodelab/app/src/React/View/Port.hs
@@ -8,22 +8,81 @@ import           React.Store       (Ref, dispatch)
 import           React.Store.Node   (Node)
 import qualified React.Store.Node   as Node
 import           Utils.PreludePlus
-import Object.Widget.Port (Port)
+import           Object.Widget.Port (Port)
 import qualified Object.Widget.Port as Port
-
+import qualified Data.JSString.Text as JS
+import qualified Numeric as Numeric
+--import qualified Data.Text as T
+--import qualified Data.Text.Internal.Lazy as TS
 
 name :: JSString
 name = "port"
 
+--pi :: Float
+--pi = 3.14159
+
+showF :: Float -> String
+showF a = Numeric.showFFloat (Just 1) a ""
 
 port :: Ref Node -> ReactView Port
 port ref = React.defineView name $ \port -> do
-    circle_
-        [ "className" $= "port port--self"
-        , "fill"      $= "#8ABEB7"
-        , "stroke"    $= "#8ABEB7"
-        ] mempty
+    portIO_
 
 
 port_ :: Ref Node -> Port -> ReactElementM ViewEventHandler ()
 port_ ref p = React.view (port ref) p mempty
+
+
+portSelf_ :: ReactElementM ViewEventHandler ()
+portSelf_ = do
+    let color = "#8ABEB7"
+    circle_
+        [ "className" $= "port port--self"
+        , "fill"      $= color
+        , "stroke"    $= color
+        ] mempty
+
+
+portIO_ :: ReactElementM ViewEventHandler ()
+portIO_ = do
+
+    let color  = "#8ABEB7"
+        inputs = 3 :: Int
+        number = 1 :: Int
+
+        line = 3 :: Float
+
+        r1   = 20 :: Float
+        r2   = r1 - line
+
+        gap  = 0.15 :: Float
+        gap' = gap * (r1/r2)
+
+        t   = pi / fromIntegral inputs
+        t1  = fromIntegral number * t - pi - t + gap/2
+        t2  = fromIntegral number * t - pi - gap/2
+        t1' = fromIntegral number * t - pi - t + gap'/2
+        t2' = fromIntegral number * t - pi - gap'/2
+
+        ax = showF $ r1 * sin(t1) + r1
+        ay = showF $ r1 * cos(t1) + r1
+
+        bx = showF $ r1 * sin(t2) + r1
+        by = showF $ r1 * cos(t2) + r1
+
+        cx = showF $ r2 * sin(t2') + r1
+        cy = showF $ r2 * cos(t2') + r1
+
+        dx = showF $ r2 * sin(t1') + r1
+        dy = showF $ r2 * cos(t1') + r1
+                                                                                                  -- v -- IO flag
+        svgPath = fromString $ "M" <> ax <> " " <> ay <> " A " <> show r1 <> " " <> show r1 <> " 1 0 0 " <> bx <> " " <> by <>
+                              " L" <> cx <> " " <> cy <> " A " <> show r2 <> " " <> show r2 <> " 1 0 1 " <> dx <> " " <> dy <>
+                              " L" <> ax <> " " <> ay
+
+    path_
+        [ "className" $= (fromString $ "port port--i port--i--" <> show number)
+        , "fill"      $= color
+        , "stroke"    $= color
+        , "d"         $= svgPath
+        ] mempty
