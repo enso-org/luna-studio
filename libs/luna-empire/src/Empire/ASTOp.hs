d273075f58bc87e9163372d1c8013f0b4c6fc6ca
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2016-12-21 16:02:57 +0100

    Remove redundancy in EmpirePass definitions

--- libs/luna-empire/src/Empire/ASTOp.hs
+++ libs/luna-empire/src/Empire/ASTOp.hs
@@ -1,58 +1,75 @@
 {-# LANGUAGE ConstraintKinds      #-}
 {-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE FlexibleContexts     #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE StandaloneDeriving   #-}
-{-# LANGUAGE TypeApplications     #-}
 
-module Empire.ASTOp where
+module Empire.ASTOp (
+    ASTOp
+  , runASTOp
+  , lams
+  ) where
 
 import           Empire.Prelude
 
-import           Control.Monad                           (foldM)
-import           Control.Monad.State                     (get, put)
-import           Control.Monad.Except                    (throwError)
-import           Empire.Data.AST                         (AST, ASTState(..), NodeRef)
-import           Empire.Data.Layers                      (Marker, Meta,
-                                                          InputsLayer, TypeLayer, TCData)
-import           Empire.Empire                           (Command)
-import qualified Luna.Pass    as Pass (SubPass, Inputs, Outputs, Preserves, Events, eval')
-import Luna.Pass.Evaluation.Interpreter.Layer (InterpreterData)
+import           Control.Monad        (foldM)
+import           Control.Monad.State  (get, put)
+import           Control.Monad.Except (throwError)
+import           Empire.Data.AST      (AST, ASTState(..), NodeRef)
+import           Empire.Data.Layers   (Marker, Meta,
+                                      InputsLayer, TypeLayer, TCData)
+import           Empire.Empire        (Command)
 
-import Data.Event (Emitter, type (//))
-import Luna.IR (IRMonad, Accessibles, ExprNet, ExprLinkNet, ExprLinkLayers, ExprLayers, Model,
-                unsafeRelayout, generalize, lam, evalIRBuilder, evalPassManager,
-                snapshot, IRBuilder, NEW, DELETE, LINK', EXPR, Abstract)
-import qualified Luna.Pass.Manager as Pass (PassManager, get)
-import qualified Luna.IR.Function as IR (Arg, arg)
-import System.Log (Logger, DropLogger, dropLogs)
-import Luna.IR.Layer.Succs (Succs)
+import           Data.Event           (Emitters, type (//))
+import           Luna.IR              (Abstract, Accessibles, IRBuilder, IRMonad,
+                                       ExprNet, ExprLinkNet, ExprLinkLayers, ExprLayers,
+                                       Model, NEW, DELETE, LINK', EXPR,
+                                       unsafeRelayout, generalize, lam, evalIRBuilder,
+                                       evalPassManager, snapshot)
+import qualified Luna.IR.Function     as IR (Arg, arg)
+import           Luna.IR.Layer.Succs  (Succs)
+import           Luna.Pass            (Inputs, Outputs, Preserves, Events)
+import qualified Luna.Pass            as Pass (SubPass, eval')
+import qualified Luna.Pass.Manager    as Pass (PassManager, get)
+import           Luna.Pass.Evaluation.Interpreter.Layer (InterpreterData)
 
-type ASTOp m = (MonadThrow m, IRMonad m, Emitter m (NEW // LINK' EXPR), Emitter m (NEW // EXPR),
-                Emitter m (DELETE // LINK' EXPR), Emitter m (DELETE // EXPR),
-                Accessibles m ('[ExprNet, ExprLinkNet] <>
-                    ExprLayers '[Model, Marker, Meta, InputsLayer, Succs, InterpreterData, TCData, TypeLayer] <>
-                    ExprLinkLayers '[Model]))
+import           System.Log (Logger, DropLogger, dropLogs)
+
+
+type ASTOp m = (MonadThrow m, IRMonad m,
+                Emitters m EmpireEmitters,
+                Accessibles m EmpireAccessibles)
+
+type EmpireAccessibles = '[ExprNet, ExprLinkNet] <>
+          ExprLayers     '[Model,
+                           Marker,
+                           Meta,
+                           InputsLayer,
+                           Succs,
+                           InterpreterData,
+                           TCData,
+                           TypeLayer] <>
+          ExprLinkLayers '[Model]
+
+type EmpireEmitters = '[NEW // LINK' EXPR,
+                        NEW // EXPR,
+                        DELETE // LINK' EXPR,
+                        DELETE // EXPR]
 
 data EmpirePass
-type instance Abstract EmpirePass = EmpirePass
-type instance Pass.Inputs  EmpirePass   = '[ExprNet, ExprLinkNet] <>
-    ExprLayers '[Model, Meta, Marker, InputsLayer, TypeLayer, Succs, InterpreterData, TypeLayer, TCData] <>
-    ExprLinkLayers '[Model]
-type instance Pass.Outputs EmpirePass   = '[ExprNet, ExprLinkNet] <>
-    ExprLayers '[Model, Meta, Marker, InputsLayer, TypeLayer, Succs, InterpreterData, TypeLayer, TCData] <>
-    ExprLinkLayers '[Model]
-type instance Pass.Events EmpirePass = '[NEW // LINK' EXPR, NEW // EXPR, DELETE // LINK' EXPR, DELETE // EXPR]
-type instance Pass.Preserves EmpirePass = '[]
+type instance Abstract  EmpirePass = EmpirePass
+type instance Inputs    EmpirePass = EmpireAccessibles
+type instance Outputs   EmpirePass = EmpireAccessibles
+type instance Events    EmpirePass = EmpireEmitters
+type instance Preserves EmpirePass = '[]
 
-runASTOp :: Pass.SubPass EmpirePass (Pass.PassManager (IRBuilder (Logger DropLogger IO))) a -> Command AST a
+runASTOp :: Pass.SubPass EmpirePass (Pass.PassManager (IRBuilder (Logger DropLogger IO))) a
+         -> Command AST a
 runASTOp pass = do
     ASTState currentStateIR currentStatePass <- get
-    (a, (st, passSt)) <- liftIO $ dropLogs $ flip evalIRBuilder currentStateIR $ flip evalPassManager currentStatePass $ do
-        a <- Pass.eval' pass
-        st <- snapshot
+    let evalIR = dropLogs . flip evalIRBuilder currentStateIR . flip evalPassManager currentStatePass
+    (a, (st, passSt)) <- liftIO $ evalIR $ do
+        a      <- Pass.eval' pass
+        st     <- snapshot
         passSt <- Pass.get
         return (a, (st, passSt))
     put $ ASTState st passSt
