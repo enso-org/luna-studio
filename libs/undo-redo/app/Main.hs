f8d436040a4339214b37eba2bca46866b3550504
Sylwia <s.brodacka@gmail.com>
2016-12-23 13:32:35 +0100

    simple main

--- libs/undo-redo/app/Main.hs
+++ libs/undo-redo/app/Main.hs
@@ -11,26 +11,17 @@ import           Version
 import qualified System.Log.Options   as Opt
 import           System.Log.Options (help, long, metavar, short)
 import qualified Undo                 as Undo
+
+import           System.Log.MLogger
 import qualified ZMQ.Bus.EndPoint     as EP
 import qualified ZMQ.Bus.Config       as Config
 
-parser :: Opt.Parser Cmd
-parser = Opt.flag' Cmd.Version (short 'V' <> long "version" <> help "Version information")
-       <|> Cmd.Run
-           <$> Opt.many         (Opt.strOption (short 't' <> metavar "TOPIC" <> help "Topic to listen"))
-           <*> Opt.optIntFlag   (Just "verbose") 'v' 2 4 "Verbosity level (0-5, default 3)"
-           <*> not . Opt.switch (long "unformatted" <> help "Unformatted output" )
+import           Control.Monad (forever)
 
-opts :: Opt.ParserInfo Cmd
-opts = Opt.info (Opt.helper <*> parser)
-                (Opt.fullDesc <> Opt.header Version.fullVersion)
+import System.IO (stdout,hFlush)
+
 
 main :: IO ()
-main = Opt.execParser opts >>= run
-
-run :: Cmd -> IO()
-run cmd = case cmd of
-    Cmd.Version  -> putStrLn Version.fullVersion
-    Cmd.Run {} -> do
-        endPoints <- EP.clientFromConfig <$> Config.load
-        Undo.runUndo endPoints Undo.empty
+main = do
+    endPoints <- EP.clientFromConfig <$> Config.load
+    Undo.runUndo endPoints Undo.empty
