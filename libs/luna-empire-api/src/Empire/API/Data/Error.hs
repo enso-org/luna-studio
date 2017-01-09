293dd15a81f2714fc95d52a2d87565703f9cb4d6
pmlodawski <pmlodawski@gmail.com>
2017-01-09 13:44:53 +0100

    Render expanded ports faster

--- libs/luna-empire-api/src/Empire/API/Data/Error.hs
+++ libs/luna-empire-api/src/Empire/API/Data/Error.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE DeriveAnyClass #-}
 module Empire.API.Data.Error where
 
-import Prologue
-import Data.Binary (Binary)
+import           Control.DeepSeq (NFData)
+import           Data.Binary     (Binary)
+import           Prologue
 
-data Error t = ImportError String | NoMethodError String t | TypeError t t | RuntimeError String deriving (Show, Eq, Generic)
+data Error t = ImportError String | NoMethodError String t | TypeError t t | RuntimeError String deriving (Show, Eq, Generic, NFData)
 
 instance Binary t => Binary (Error t)
