30efed65de05644d4a01ee262117e5652b7dad03
Jan Kłosowski <janklosowski@gmail.com>
2017-01-14 17:28:20 +0100

    Connection drawing refactor Closes T158

--- nodelab/app/src/Luna/Studio/React/View/Connection.hs
+++ nodelab/app/src/Luna/Studio/React/View/Connection.hs
@@ -4,7 +4,7 @@ module Luna.Studio.React.View.Connection where
 import           Empire.API.Data.PortRef            (InPortRef)
 import qualified Event.UI                           as UI
 import           Luna.Studio.Action.Geometry        (connectionWidth)
-import           Luna.Studio.Data.Color             (toJSString, Color)
+import           Luna.Studio.Data.Color             (toJSString)
 import           Luna.Studio.Data.Vector            (x, y, Position, averagePosition)
 import           Luna.Studio.Prelude
 import           Luna.Studio.React.Event.Connection (ModifiedEnd (Destination, Source))
@@ -24,32 +24,21 @@ name = "connection-editor"
 show2 :: Double -> String
 show2 a = showFFloat (Just 2) a "" -- limit Double to two decimal numbers
 
-
---FIXME: Why it does not work with [PropertyOrHandler handler]
+--TODO: move & refactor: the list is inversed
 mergeList :: [a] -> [a] -> [a]
-mergeList xs      [] = xs
-mergeList []      ys = ys
 mergeList []      [] = []
-mergeList (x: xs) ys = mergeList xs (x:ys)
+mergeList []      ys = ys
+mergeList xs      [] = xs
+mergeList (x:xs) ys = mergeList xs (x:ys)
 
---FIXME: Position -> Position -> [PropertyOrHandler handler] -> ReactElementM ViewEventHandler ()
-lineReact :: Position -> Position -> Maybe Double -> Maybe Color -> Maybe (PropertyOrHandler ViewEventHandler) -> ReactElementM ViewEventHandler ()
-lineReact src dst mwidth mcolor mevent = do
+lineReact :: Position -> Position -> [PropertyOrHandler ViewEventHandler] -> ReactElementM ViewEventHandler ()
+lineReact src dst b = do
     let a = [ "x1" $= (fromString $ show2 $ src ^. x)
             , "y1" $= (fromString $ show2 $ src ^. y)
             , "x2" $= (fromString $ show2 $ dst ^. x)
             , "y2" $= (fromString $ show2 $ dst ^. y)
             ]
-        b = case mwidth of
-            Nothing      -> a
-            (Just width) -> ("strokeWidth" $= (fromString $ show width)):a
-        c = case mcolor of
-            Nothing      -> b
-            (Just color) -> ("stroke" $= (toJSString color)):b
-        d = case mevent of
-            Nothing      -> c
-            (Just event) -> (event):c
-    line_ d mempty
+    line_ (mergeList a b) mempty
 
 connection :: ReactView (Ref App, Connection)
 connection = React.defineView name $ \(ref, model) -> do
@@ -57,31 +46,29 @@ connection = React.defineView name $ \(ref, model) -> do
             src         = model ^. Connection.from
             dst         = model ^. Connection.to
             mid         = averagePosition src dst
-            color       = model ^. Connection.color
-            width       = connectionWidth
-            widthSelect = connectionWidth * 4
-            event1      = onMouseDown $ \e m -> stopPropagation e : dispatch ref (UI.ConnectionEvent $ Connection.ModifyConnection m connId Source)
-            event2      = onMouseDown $ \e m -> stopPropagation e : dispatch ref (UI.ConnectionEvent $ Connection.ModifyConnection m connId Destination)
+            color       = "stroke"      $= (toJSString $ model ^. Connection.color  )
+            width       = "strokeWidth" $= (fromString $ show2   connectionWidth    )
+            widthSelect = "strokeWidth" $= (fromString $ show2 $ connectionWidth * 4)
+            eventSrc    = onMouseDown $ \e m -> stopPropagation e : dispatch ref (UI.ConnectionEvent $ Connection.ModifyConnection m connId Source)
+            eventDst    = onMouseDown $ \e m -> stopPropagation e : dispatch ref (UI.ConnectionEvent $ Connection.ModifyConnection m connId Destination)
         g_ [ "className" $= "connection" ] $ do
             g_ [ "className" $= "connection__src" ] $ do
-                lineReact src mid (Just width      ) (Just color) Nothing
-                lineReact src mid (Just widthSelect) Nothing      (Just event1)
+                lineReact src mid [ width, color ]
+                lineReact src mid [ widthSelect, eventSrc ]
             g_ [ "className" $= "connection__dst" ] $ do
-                lineReact mid dst (Just width      ) (Just color) Nothing
-                lineReact mid dst (Just widthSelect) Nothing      (Just event2)
+                lineReact mid dst [ width, color ]
+                lineReact mid dst [ widthSelect, eventDst ]
 
 connection_ :: Ref App -> InPortRef -> Connection -> ReactElementM ViewEventHandler ()
 connection_ ref inPortRef model = React.viewWithSKey connection (fromString $ show inPortRef) (ref, model) mempty
-
 
 currentConnection :: ReactView CurrentConnection
 currentConnection = React.defineView name $ \model -> do
         let src   = model ^. Connection.currentFrom
             dst   = model ^. Connection.currentTo
-            color = model ^. Connection.currentColor
-            width = connectionWidth
-        lineReact src dst (Just width) (Just color) Nothing
-
+            color = "stroke"      $= (toJSString $ model ^. Connection.currentColor)
+            width = "strokeWidth" $= (fromString $ show2 connectionWidth           )
+        lineReact src dst [ width, color ]
 
 currentConnection_ :: CurrentConnection -> ReactElementM ViewEventHandler ()
 currentConnection_ model = React.viewWithSKey currentConnection "current-connection" model mempty
