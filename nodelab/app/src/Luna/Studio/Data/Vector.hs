2c107b22f4271068f33a52b7a0663501bf1d2824
Jan Kłosowski <janklosowski@gmail.com>
2017-01-15 13:18:54 +0100

    averagePosition refactor Ref T158

--- nodelab/app/src/Luna/Studio/Data/Vector.hs
+++ nodelab/app/src/Luna/Studio/Data/Vector.hs
@@ -163,13 +163,9 @@ instance IsList Position where
 --TODO: [Position] -> Position
 averagePosition :: Position -> Position -> Position
 averagePosition a b =
-    let x1 = a ^. x
-        y1 = a ^. y
-        x2 = b ^. x
-        y2 = b ^. y
-        x3 = (x1 + x2) / 2
-        y3 = (y1 + y2) / 2
-    in Position (Vector2 x3 y3)
+    let ax = (a ^. x + b ^. x) / 2
+        ay = (a ^. y + b ^. y) / 2
+    in Position (Vector2 ax ay)
 
 move :: Position -> Vector2 Double -> Position
 move pos vec = pos & vector +~ vec
