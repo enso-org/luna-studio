9921f5d79a14c345afe54072b20bd67421b09f10
Jan Kłosowski <janklosowski@gmail.com>
2017-01-17 17:26:32 +0100

    code beautifying

--- nodelab/app/src/Luna/Studio/React/View/Visualization/DataFrame.hs
+++ nodelab/app/src/Luna/Studio/React/View/Visualization/DataFrame.hs
@@ -3,25 +3,27 @@ module Luna.Studio.React.View.Visualization.DataFrame where
 
 import qualified Data.Text                         as Text
 import           React.Flux
-
 import           Luna.Studio.Prelude
 import           Luna.Studio.React.Model.DataFrame (DataFrame)
 import qualified Luna.Studio.React.Model.DataFrame as DataFrame
-
 
 
 dataFrame_ :: Int -> DataFrame -> ReactElementM ViewEventHandler ()
 dataFrame_ visIx df =
     div_
         [ "key" $= fromString (show visIx)
-        , "className" $= "vis vis--table" ] $ do
-        div_ [ "className" $= "blur" ] mempty
-        table_ $
-            thead_ $
+        , "className" $= "vis vis--table"
+        ] $ do
+        div_
+            [ "className" $= "blur" ] mempty
+        table_ $ do
+            thead_ $ do
                 tr_ $ forM_ (df ^. DataFrame.headers) $
                     th_ .
                         elemString . fromString . Text.unpack
-        div_ [ "className" $= "scroll" ] $
+        div_
+            [ "className" $= "scroll"
+            ] $ do
             table_ $
                 tbody_ $
                     forM_ (df ^. DataFrame.rows) $ \row ->
