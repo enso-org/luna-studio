44c6238949b403f05de64124593c992786b19dce
Sylwia Brodacka <sylwiabrodacka@MacBook-Pro-Sylwia.local>
2017-02-01 17:21:32 +0100

    remove unused imports form Main

--- libs/undo-redo/app/Main.hs
+++ libs/undo-redo/app/Main.hs
@@ -4,18 +4,10 @@ module Main where
 
 import           Prologue
 
-import qualified System.Log.Options   as Opt
-import           System.Log.Options (help, long, metavar, short)
 import qualified Undo                 as Undo
 
-import           System.Log.MLogger
 import qualified ZMQ.Bus.EndPoint     as EP
 import qualified ZMQ.Bus.Config       as Config
-
-import           Control.Monad (forever)
-
-import System.IO (stdout,hFlush)
-
 
 main :: IO ()
 main = do
