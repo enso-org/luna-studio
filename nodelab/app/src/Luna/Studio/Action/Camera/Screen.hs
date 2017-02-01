946a54a8772b24a64f24c22d5f201835f571183e
Piotr Młodawski <pmlodawski@gmail.com>
2017-02-01 10:21:49 +0100

    Fix performance issue when moving nodes

--- nodelab/app/src/Luna/Studio/Action/Camera/Screen.hs
+++ nodelab/app/src/Luna/Studio/Action/Camera/Screen.hs
@@ -8,9 +8,10 @@ module Luna.Studio.Action.Camera.Screen
 
 import           Data.Matrix                           (getElem, multStd2)
 import qualified Data.Matrix                           as Matrix
-import           Data.Position                         (Position (Position), ScreenPosition, Vector2 (Vector2))
-import           Data.Size                             (Size (Size))
+import           Data.Position                         (Position (Position), ScreenPosition)
+import           Data.Size                             (Size)
 import           Data.Vector                           (fromTuple, scalarProduct, vector, x, y)
+import qualified JS.Scene                              as Scene
 import           Luna.Studio.Action.Command            (Command)
 import           Luna.Studio.Data.CameraTransformation (screenToLogical)
 import           Luna.Studio.Prelude
@@ -20,22 +21,17 @@ import qualified Luna.Studio.State.Global              as Global
 
 
 
-foreign import javascript safe "document.getElementById('Graph').offsetWidth"  workspaceWidth  :: IO Double
-foreign import javascript safe "document.getElementById('Graph').offsetHeight" workspaceHeight :: IO Double
-foreign import javascript safe "document.getElementById('Graph').getBoundingClientRect().left" workspaceLeft :: IO Double
-foreign import javascript safe "document.getElementById('Graph').getBoundingClientRect().top"  workspaceTop  :: IO Double
+getWorkspacePos :: Command State Position
+getWorkspacePos = use $ Global.scene . Scene.position
 
-getWorkspacePos :: MonadIO m => m Position
-getWorkspacePos = Position .: Vector2 <$> liftIO workspaceLeft <*> liftIO workspaceTop
-
-getScreenCenter :: MonadIO m => m ScreenPosition
+getScreenCenter :: Command State ScreenPosition
 getScreenCenter = getScreenCenterFromSize <$> getScreenSize
 
 getScreenCenterFromSize :: Size -> ScreenPosition
 getScreenCenterFromSize = Position . flip scalarProduct 0.5 . view vector
 
-getScreenSize :: MonadIO m => m Size
-getScreenSize = liftIO $ Size . fromTuple <$> ((,) <$> workspaceWidth <*> workspaceHeight)
+getScreenSize :: Command State Size
+getScreenSize = use $ Global.scene . Scene.size
 
 translateToWorkspace :: ScreenPosition -> Command State Position
 translateToWorkspace pos = do
