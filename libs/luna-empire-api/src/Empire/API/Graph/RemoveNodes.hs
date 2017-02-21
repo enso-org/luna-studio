c9f2ef908a477da1845e70f85fea87ce64e8f00d
Piotr Młodawski <pmlodawski@gmail.com>
2017-02-21 12:57:27 +0100

    Remove unused imports

--- libs/luna-empire-api/src/Empire/API/Graph/RemoveNodes.hs
+++ libs/luna-empire-api/src/Empire/API/Graph/RemoveNodes.hs
@@ -11,8 +11,8 @@ import           Empire.API.Data.TypeRep       (TypeRep)
 import qualified Empire.API.Graph.Request      as G
 import qualified Empire.API.Request            as R
 import qualified Empire.API.Response           as Response
-import qualified Empire.API.Response           as Response
 import qualified Empire.API.Topic              as T
+
 
 data Request = Request { _location :: GraphLocation
                        , _nodeIds  :: [NodeId]
