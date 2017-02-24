de076f028a7b2f391abc5ec8d138892e92f0d1c0
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2017-02-24 17:35:58 +0100

    Fix updating node expression Fix T277

--- libs/batch/plugins/luna-empire/src/Empire/Server/Graph.hs
+++ libs/batch/plugins/luna-empire/src/Empire/Server/Graph.hs
@@ -203,7 +203,7 @@ handleRemoveNodes = modifyGraphOk action success where
 handleUpdateNodeExpression :: Request UpdateNodeExpression.Request -> StateT Env BusT ()-- fixme [SB] returns Result with no new informations and change node expression has addNode+removeNodes
 handleUpdateNodeExpression = modifyGraph action success where
     action (UpdateNodeExpression.Request location nodeId expression) = do
-        oldExpr <- Graph.withGraph location $ $notImplemented -- GraphUtils.getASTTarget nodeId >>= Print.printNodeExpression
+        oldExpr <- Graph.withGraph location $ runASTOp $ GraphUtils.getASTTarget nodeId >>= Print.printNodeExpression
         let newNodeId = nodeId
             inv = UpdateNodeExpression.Inverse (Text.pack oldExpr)
             res = Graph.updateNodeExpression location nodeId newNodeId expression
