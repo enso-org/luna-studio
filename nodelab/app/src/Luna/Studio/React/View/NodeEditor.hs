6c60cfcaf93ec480ff4c50a70d867f85913c3f15
LJK <leszek.kania@luna-lang.org>
2017-02-27 13:36:14 +0100

    Fix to incorrectly displayed eges

--- nodelab/app/src/Luna/Studio/React/View/NodeEditor.hs
+++ nodelab/app/src/Luna/Studio/React/View/NodeEditor.hs
@@ -11,11 +11,13 @@ import qualified Luna.Studio.Event.UI                  as UI
 import           Luna.Studio.Prelude                   hiding (transform)
 import qualified Luna.Studio.React.Event.NodeEditor    as NE
 import           Luna.Studio.React.Model.App           (App)
+import           Luna.Studio.React.Model.Node          (isEdge)
 import           Luna.Studio.React.Model.NodeEditor    (NodeEditor)
 import qualified Luna.Studio.React.Model.NodeEditor    as NodeEditor
 import           Luna.Studio.React.Store               (Ref, dispatch)
 import           Luna.Studio.React.View.Connection     (connection_, currentConnection_)
 import           Luna.Studio.React.View.ConnectionPen  (connectionPen_)
+import           Luna.Studio.React.View.Edge           (edgeSidebar_)
 import           Luna.Studio.React.View.Monad          (monad_)
 import           Luna.Studio.React.View.Node           (nodeDynamicStyles_, node_)
 import           Luna.Studio.React.View.Searcher       (searcher_)
@@ -34,10 +36,10 @@ nodeEditor_ ref ne = React.viewWithSKey nodeEditor name (ref, ne) mempty
 
 nodeEditor :: ReactView (Ref App, NodeEditor)
 nodeEditor = React.defineView name $ \(ref, ne) -> do
-    let camera     = ne ^. NodeEditor.screenTransform . CameraTransformation.logicalToScreen
-        nodes      = ne ^. NodeEditor.nodes
-        lookupNode = _2 %~ mapMaybe (flip HashMap.lookup nodes)
-        monads     = map lookupNode $ ne ^. NodeEditor.monads
+    let camera         = ne ^. NodeEditor.screenTransform . CameraTransformation.logicalToScreen
+        (edges, nodes) = partition isEdge $ ne ^. NodeEditor.nodes . to HashMap.elems
+        lookupNode     = _2 %~ mapMaybe (flip HashMap.lookup $ ne ^. NodeEditor.nodes)
+        monads         = map lookupNode $ ne ^. NodeEditor.monads
     div_
         [ "className" $= "luna-graph"
         , "id"        $= sceneId
@@ -94,9 +96,10 @@ nodeEditor = React.defineView name $ \(ref, ne) -> do
             [ "className" $= "luna-plane luna-plane--nodes"
             , "key"       $= "nodes"
             ] $ do
-            forM_ (HashMap.elems nodes) $ node_ ref
+            forM_ nodes $ node_ ref
             forM_ (ne ^. NodeEditor.visualizations) $ pinnedVisualization_ ref ne
             mapM_ (searcher_ ref camera) $ ne ^. NodeEditor.searcher
+        forM_ edges $ edgeSidebar_ ref (ne ^. NodeEditor.draggedPort)
         canvas_
             [ "className" $= "luna-plane plane--canvas luna-hide"
             , "key"       $= "canvas"
