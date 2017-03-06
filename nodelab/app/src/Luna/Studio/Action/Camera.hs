bc8334119331ad907747be935d8d58556df03b03
Piotr Młodawski <pmlodawski@gmail.com>
2017-03-06 16:17:29 +0100

    Select most center node when nothing is selected and navigation event occur.

--- nodelab/app/src/Luna/Studio/Action/Camera.hs
+++ nodelab/app/src/Luna/Studio/Action/Camera.hs
@@ -15,6 +15,7 @@ module Luna.Studio.Action.Camera
      , getWorkspacePos
      , translateToScreen
      , translateToWorkspace
+     , getScreenCenter
      , startZoomDrag
      , zoomDrag
      , wheelZoom
@@ -26,5 +27,5 @@ import           Luna.Studio.Action.Camera.Center (centerGraph)
 import           Luna.Studio.Action.Camera.Modify (resetCamera)
 import           Luna.Studio.Action.Camera.Pan    (panCamera, panDown, panDrag, panLeft, panRight, panUp, resetPan, startPanDrag,
                                                    stopPanDrag)
-import           Luna.Studio.Action.Camera.Screen (getWorkspacePos, translateToScreen, translateToWorkspace)
+import           Luna.Studio.Action.Camera.Screen (getScreenCenter, getWorkspacePos, translateToScreen, translateToWorkspace)
 import           Luna.Studio.Action.Camera.Zoom   (resetZoom, startZoomDrag, stopZoomDrag, wheelZoom, zoomDrag, zoomIn, zoomOut)
