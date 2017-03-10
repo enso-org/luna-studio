20f9c3317fdbc780de2a3ef02b887d6e07b8cb63
Marcin Kostrzewa <marcinkostrzewa@yahoo.com>
2017-03-10 16:34:51 +0100

    delete commented code

--- libs/luna-empire/src/Empire/Data/BreadcrumbHierarchy.hs
+++ libs/luna-empire/src/Empire/Data/BreadcrumbHierarchy.hs
@@ -31,16 +31,6 @@ makeLenses ''BItem
 instance Default BItem where
     def = BItem def def def def
 
-{-replaceAt :: Breadcrumb BreadcrumbItem -> BreadcrumbHierarchy -> BreadcrumbHierarchy -> Maybe BreadcrumbHierarchy-}
-{-replaceAt (Breadcrumb bs) (BC new) (BC hierarchy) = BC <$> go bs new hierarchy-}
-    {-where-}
-      {-go :: [BreadcrumbItem] -> Map NodeId BItem -> Map NodeId BItem -> Maybe (Map NodeId BItem)-}
-      {-go [] new _ = pure new-}
-      {-go (Lambda b : bs) new current = do-}
-          {-item <- Map.lookup b current-}
-          {-n    <- go bs new $ item ^. children . items-}
-          {-return $ current & children . items . at b ?~ n-}
-
 navigateTo :: BItem -> Breadcrumb BreadcrumbItem -> Maybe BItem
 navigateTo b (Breadcrumb crumbs) = go crumbs b where
     go [] b = pure b
@@ -58,31 +48,3 @@ replaceAt (Breadcrumb crumbs) par child = go crumbs par child where
 
 topLevelIDs :: BItem -> [NodeId]
 topLevelIDs = Map.keys . view children
-
-{-navigateTo :: BreadcrumbHierarchy -> Breadcrumb BreadcrumbItem -> Maybe BreadcrumbHierarchy-}
-{-navigateTo (BC forest) (Breadcrumb breadcrumbs) = BC <$> go forest nodeIds-}
-    {-where-}
-      {-nodeIds = map (\(Lambda id) -> id) breadcrumbs-}
-      {-go :: Forest NodeId -> [NodeId] -> Maybe (Forest NodeId)-}
-      {-go forest [] = Just forest-}
-      {-go forest (b:bs) | Just f <- at b forest = go f bs-}
-                       {-| otherwise = Nothing-}
-
-{-at :: NodeId -> Forest NodeId -> Maybe (Forest NodeId)-}
-{-at nid forest = snd <$> find ((==) nid . fst) topLevel-}
-    {-where-}
-      {-topLevel = map (\a -> (rootLabel a, subForest a)) forest-}
-
-{-addID :: NodeId -> BreadcrumbHierarchy -> BreadcrumbHierarchy-}
-{-addID nodeid (BC hierarchy) = BC $ Node nodeid [] : hierarchy-}
-
-{-addWithLeafs :: NodeId -> [NodeId] -> BreadcrumbHierarchy -> BreadcrumbHierarchy-}
-{-addWithLeafs nodeid leafs (BC hierarchy) = BC $ Node nodeid children : hierarchy-}
-    {-where-}
-        {-children = map (\nid -> Node nid []) leafs-}
-
-{-removeID :: NodeId -> BreadcrumbHierarchy -> BreadcrumbHierarchy-}
-{-removeID nodeid (BC hierarchy) = BC $ filter (\(Node nid _) -> nid /= nodeid) hierarchy-}
-
-{-topLevelIDs :: BreadcrumbHierarchy -> [NodeId]-}
-{-topLevelIDs (BC f) = map (\(Node nid _) -> nid) f-}
