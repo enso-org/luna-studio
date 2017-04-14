d4fa60206a1f443cf94a772ea83add040d281f22
Sylwia <s.brodacka@gmail.com>
2017-04-14 11:48:38 +0200

    remove unused files from internals

--- internals/app/js/env-interactive.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = function() {
-  // required for interactive
-  window.app            = require('./app-internals');
-  window.common         = require('./common');
-  window.config         = require('./config');
-};
