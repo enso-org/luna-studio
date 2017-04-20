dc34efb55e2ad8bc03bddbac4023e42c2e7aafe7
Sylwia <s.brodacka@gmail.com>
2017-04-20 09:59:17 +0200

    `stack build` without `--fast` for local builds

--- luna-studio/script/atom_install.py
+++ luna-studio/script/atom_install.py
@@ -6,7 +6,7 @@ import subprocess
 
 nodelab_dir = atom_prepare.prep_path('..')
 os.chdir(nodelab_dir)
-subprocess.call(['stack', 'build', '--fast'])
+subprocess.call(['stack', 'build'])
 atom_prepare.main()
 os.environ['ATOM_HOME'] = os.environ.get('LUNA_HOME', os.path.expanduser('~') + '/.luna-atom')
 atom_dir = atom_prepare.prep_path('../atom')
