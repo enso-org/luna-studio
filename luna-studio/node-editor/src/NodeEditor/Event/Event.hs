42971a82f4e13da8e7543bb73d4bc949a2948dec
Piotr Młodawski <pmlodawski@gmail.com>
2017-05-17 19:38:55 +0200

    Remove Debug event

--- luna-studio/node-editor/src/NodeEditor/Event/Event.hs
+++ luna-studio/node-editor/src/NodeEditor/Event/Event.hs
@@ -2,12 +2,10 @@
 module NodeEditor.Event.Event where
 
 import           Data.Aeson                  (ToJSON)
-
 import           Common.Prelude
 import qualified NodeEditor.Event.Atom       as Atom
 import qualified NodeEditor.Event.Batch      as Batch
 import qualified NodeEditor.Event.Connection as Connection
-import qualified NodeEditor.Event.Debug      as Debug
 import           NodeEditor.Event.Shortcut   (ShortcutEvent)
 import           NodeEditor.Event.UI         (UIEvent)
 
@@ -15,7 +13,6 @@ import           NodeEditor.Event.UI         (UIEvent)
 data Event = Init
            | Batch                         Batch.Event
            | Connection               Connection.Event
-           | Debug                         Debug.Event
            | Atom                           Atom.Event
            | Shortcut                    ShortcutEvent
            | UI                                UIEvent
