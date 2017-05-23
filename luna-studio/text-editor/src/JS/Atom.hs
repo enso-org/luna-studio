4deddbb194a81e1e13e8f02636ede8e670349877
Piotr Młodawski <pmlodawski@gmail.com>
2017-05-23 09:26:02 +0200

    Code formatting

--- luna-studio/text-editor/src/JS/Atom.hs
+++ luna-studio/text-editor/src/JS/Atom.hs
@@ -9,13 +9,13 @@ module JS.Atom
     ) where
 
 
-import           GHCJS.Foreign.Callback
-import           GHCJS.Marshal.Pure         (pFromJSVal)
-import qualified Data.Text                  as Text
-import           TextEditor.Event.Internal (InternalEvent, InternalEvent(..))
-import           TextEditor.Event.Text (TextEvent, TextEvent (..))
-import qualified TextEditor.Event.Text as TextEvent
 import           Common.Prelude
+import qualified Data.Text                 as Text
+import           GHCJS.Foreign.Callback
+import           GHCJS.Marshal.Pure        (pFromJSVal, pToJSVal)
+import           TextEditor.Event.Internal (InternalEvent, InternalEvent (..))
+import           TextEditor.Event.Text     (TextEvent, TextEvent (..))
+import qualified TextEditor.Event.Text     as TextEvent
 
 
 foreign import javascript safe "atomCallbackTextEditor.pushCode($1, $2, $3, $4)"
@@ -28,46 +28,46 @@ foreign import javascript safe "atomCallbackTextEditor.pushStatus($1, $2, $3)"
     pushStatus :: JSString -> JSString -> JSString -> IO ()
 
 foreign import javascript safe "atomCallbackTextEditor.subscribeEventListenerInternal($1)"
-  subscribeEventListenerInternal' :: Callback (JSVal -> IO ()) -> IO ()
+    subscribeEventListenerInternal' :: Callback (JSVal -> IO ()) -> IO ()
 
 foreign import javascript safe "($1).unsubscribeEventListenerInternal()"
-  unsubscribeEventListenerInternal' :: Callback (JSVal -> IO ()) -> IO ()
+    unsubscribeEventListenerInternal' :: Callback (JSVal -> IO ()) -> IO ()
 
 foreign import javascript safe "atomCallbackTextEditor.subscribeText($1)"
-  subscribeText' :: Callback (JSVal -> IO ()) -> IO ()
+    subscribeText' :: Callback (JSVal -> IO ()) -> IO ()
 
 foreign import javascript safe "($1).unsubscribeText()"
-  unsubscribeText' :: Callback (JSVal -> IO ()) -> IO ()
+    unsubscribeText' :: Callback (JSVal -> IO ()) -> IO ()
 
 foreign import javascript safe "atomCallbackTextEditor.getEvent($1)"
-  getEvent :: JSVal -> JSVal
+    getEvent :: JSVal -> JSVal
 
 foreign import javascript safe "atomCallbackTextEditor.getPath($1)"
-  getPath :: JSVal -> JSVal
+    getPath :: JSVal -> JSVal
 
 foreign import javascript safe "atomCallbackTextEditor.getStart($1)"
-  getStart :: JSVal -> JSVal
+    getStart :: JSVal -> JSVal
 
 foreign import javascript safe "atomCallbackTextEditor.getStop($1)"
-  getStop :: JSVal -> JSVal
+    getStop :: JSVal -> JSVal
 
 foreign import javascript safe "atomCallbackTextEditor.getText($1)"
-  getText :: JSVal -> JSVal
+    getText :: JSVal -> JSVal
 
 foreign import javascript safe "atomCallbackTextEditor.getCursor($1)"
-  getCursor :: JSVal -> JSVal
+    getCursor :: JSVal -> JSVal
 
-foreign import javascript safe "atomCallbackInternals.getSelections($1)"
-  getSelections :: JSVal -> JSVal
+foreign import javascript safe "atomCallbackTextEditor.getSelections($1)"
+    getSelections :: JSVal -> JSVal
 
 jsvalToText :: JSVal -> TextEvent
 jsvalToText jsval = result where
-  filepath = pFromJSVal $ getPath jsval
-  start    = pFromJSVal $ getStart jsval
-  stop     = pFromJSVal $ getStop jsval
-  text     = pFromJSVal $ getText jsval
-  cursor   = pFromJSVal $ getCursor jsval
-  result   = TextEvent filepath start stop text $ Just cursor
+    filepath = pFromJSVal $ getPath jsval
+    start    = pFromJSVal $ getStart jsval
+    stop     = pFromJSVal $ getStop jsval
+    text     = pFromJSVal $ getText jsval
+    cursor   = pFromJSVal $ getCursor jsval
+    result   = TextEvent filepath start stop text $ Just cursor
 
 jsvalToInternalEvent :: JSVal -> InternalEvent
 jsvalToInternalEvent jsval = result where
@@ -78,17 +78,17 @@ jsvalToInternalEvent jsval = result where
 
 subscribeText :: (TextEvent -> IO ()) -> IO (IO ())
 subscribeText callback = do
-  wrappedCallback <- syncCallback1 ContinueAsync $ callback . jsvalToText
-  subscribeText' wrappedCallback
-  return $ unsubscribeText' wrappedCallback >> releaseCallback wrappedCallback
+    wrappedCallback <- syncCallback1 ContinueAsync $ callback . jsvalToText
+    subscribeText' wrappedCallback
+    return $ unsubscribeText' wrappedCallback >> releaseCallback wrappedCallback
 
 pushCode :: TextEvent -> IO ()
 pushCode = do
-  uri   <- (^. TextEvent.filePath)
-  start <- (^. TextEvent.start)
-  end   <- (^. TextEvent.stop)
-  text  <- (^. TextEvent.text)
-  return $ pushCode' (convert uri) start end $ convert $ Text.unpack text
+    uri   <- (^. TextEvent.filePath)
+    start <- (^. TextEvent.start)
+    end   <- (^. TextEvent.stop)
+    text  <- (^. TextEvent.text)
+    return $ pushCode' (convert uri) start end $ convert $ Text.unpack text
 
 subscribeEventListenerInternal :: (InternalEvent -> IO ()) -> IO (IO ())
 subscribeEventListenerInternal callback = do
