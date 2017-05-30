a14dde2a80d0007849d04eb1b24b07b2cbbe9116
Jan Kłosowski <janklosowski@gmail.com>
2017-05-30 02:01:33 +0200

    Searcher styles Ref T114

--- libs/luna-studio-common/src/LunaStudio/Data/Constants.hs
+++ libs/luna-studio-common/src/LunaStudio/Data/Constants.hs
@@ -2,8 +2,7 @@ module LunaStudio.Data.Constants where
 
 import           Prologue
 
-gridSize, nodeWidth, nodePropertiesWidth, gapBetweenNodes :: Double
+gridSize, nodePropertiesWidth, gapBetweenNodes :: Double
 gridSize            = 16
-nodeWidth           = 240
-nodePropertiesWidth = 220
+nodePropertiesWidth = 160
 gapBetweenNodes     = 8 * gridSize --256
