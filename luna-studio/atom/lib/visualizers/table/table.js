c38367b9eb85f0025456a00565161a03e6408509
Marcin Kostrzewa <marcinkostrzewa@yahoo.com>
2017-06-01 11:55:55 +0200

    some styles for tables

---
+++ luna-studio/atom/lib/visualizers/table/table.js
@@ -0,0 +1,13 @@
+(function () {
+  var render = function (json) {
+    data = JSON.parse(json);
+    result = "";
+    data.forEach(function (point, ix) {
+      result += ("<tr><td>" + ix + "</td><td>" + point + "</td></tr>");
+    });
+    document.body.innerHTML = "<table>" + result + "</table>";
+  };
+  window.addEventListener("message", function (evt) {
+    render(evt.data);
+  });
+}());
