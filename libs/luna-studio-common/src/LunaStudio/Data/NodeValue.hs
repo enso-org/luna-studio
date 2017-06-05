6be58a7075e2c0e806e4e85173c9abb7b71cc320
LJK <leszek.kania@luna-lang.org>
2017-06-05 12:19:33 +0200

    Handle multiple visualizers from the same module in gui

--- libs/luna-studio-common/src/LunaStudio/Data/NodeValue.hs
+++ libs/luna-studio-common/src/LunaStudio/Data/NodeValue.hs
@@ -2,11 +2,13 @@
 module LunaStudio.Data.NodeValue where
 
 import qualified Data.Aeson                 as Aeson
+import           Data.Aeson.Types           (FromJSON, ToJSON)
 import           Data.Binary                (Binary)
 import qualified Data.ByteString.Lazy.Char8 as BS
 import           Data.Map                   (Map)
 import qualified Data.Map                   as Map
 import           Data.Text                  (Text)
+import qualified Data.Text                  as Text
 import           Data.UUID.Types            (UUID)
 import           LunaStudio.Data.Error      (Error)
 import           LunaStudio.Data.TypeRep    (TypeRep, toConstructorRep)
@@ -15,23 +17,32 @@ import           Prologue                   hiding (Text, TypeRep)
 
 type VisualizerName    = Text
 type VisualizerPath    = Text
-type VisualizerMatcher = TypeRep -> IO (Maybe VisualizerPath)
+type VisualizerMatcher = TypeRep -> IO [VisualizerEntry]
 type Visualizer        = (VisualizerName, VisualizerPath)
 type VisualizationData = [Text]
 type VisualizationId   = UUID
 
-transformJSVisualizerMatcher :: MonadIO m => (String -> m String) -> TypeRep -> m (Maybe VisualizerPath)
+data VisualizerEntry = VisualizerEntry { name :: Maybe VisualizerName
+                                       , path :: VisualizerPath
+                                       } deriving (Eq, Generic, NFData, Show)
+
+instance FromJSON VisualizerEntry
+instance ToJSON   VisualizerEntry
+
+transformJSVisualizerMatcher :: MonadIO m => (String -> m String) -> TypeRep -> m [VisualizerEntry]
 transformJSVisualizerMatcher f r = case toConstructorRep r of
-    Nothing -> return Nothing
-    Just r' -> Aeson.decode . BS.pack <$> f (BS.unpack $ Aeson.encode r')
+    Nothing -> return def
+    Just r' -> fromMaybe def . Aeson.decode . BS.pack <$> f (BS.unpack $ Aeson.encode r')
 
 fromJSVisualizersMap :: Map String (String -> IO String) -> Map VisualizerName VisualizerMatcher
 fromJSVisualizersMap = Map.fromList . map convertEntry . Map.toList where
     convertEntry (k, v) = (convert k, transformJSVisualizerMatcher v)
 
 applyType :: MonadIO m => TypeRep -> Map VisualizerName VisualizerMatcher -> m (Map VisualizerName VisualizerPath)
-applyType tpe = fmap (Map.fromList . catMaybes) . mapM applyToEntry . Map.toList where
-    applyToEntry (k, f) = (k,) `fmap2` liftIO (f tpe)
+applyType tpe = fmap (Map.fromList . concat) . liftIO . mapM applyToEntry . Map.toList where
+    applyToEntry (k, f) = liftIO $ map (convertToEntry k) <$> f tpe
+    convertToEntry k (VisualizerEntry Nothing  p) = (k, p)
+    convertToEntry k (VisualizerEntry (Just n) p) = (Text.concat [k, convert ": ", n], p)
 
 type ShortValue = Text
 
