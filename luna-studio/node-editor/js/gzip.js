0f46c52306ac33cc3f29e2adcbb1ded9f25d988a
piotrMocz <piotr.moczurad@gmail.com>
2017-07-25 11:53:11 +0200

    Further cleanup, compilation flags

--- luna-studio/node-editor/js/gzip.js
+++ luna-studio/node-editor/js/gzip.js
@@ -1,20 +1,12 @@
 var pako = require('pako');
 
 var compressBytes = function (bytes) {
-    console.log('----- compressing ------');
-    console.log(bytes);
     var compressed = pako.gzip(bytes);
-    console.log(compressed);
-    console.log('----- done -------------');
     return compressed.buffer;
 };
 
 var decompressBytes = function (bytes) {
-    console.log('----- decompressing ------');
-    console.log(bytes);
     var decompressed = pako.ungzip(bytes);
-    console.log(decompressed);
-    console.log('----- done ------');
     return decompressed.buffer;
 };
 
