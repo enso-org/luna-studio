4dfd3addfa3a531d399fd7ebe53fb50c8872bfb6
ci <ci@luna-lang.org>
2017-12-06 13:41:54 +0100

    replace in atom

--- scripts_build/atom_apm.py
+++ scripts_build/atom_apm.py
@@ -186,8 +186,7 @@ def apm_packages():
 
 def modify_atom_package_json():
     json = get_path('package_json')
-    
-    # replace('"name":"atom","productName":"Atom"','"name":"luna-studio","productName":"LunaStudio"', json)
+    replace('"name":"atom","productName":"Atom"','"name":"luna-studio","productName":"LunaStudio"', json)
             # sources.write(re.sub(r'"name":"atom","productName":"Atom"', r'"name":"luna-studio","productName":"LunaStudio"', line))
     # run_process('sed', '-i', 's/"name":"atom"/"name":"LunaStudio"/g ; s/"productName":"Atom"/"productName":"LunaStudio"/g',json)
 
