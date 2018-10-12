b886007a8b2fed064fd56e05f02542c8a7fb73cc
Marcin Mikołajczyk <marcin.mikolajczyk@luna-lang.org>
2018-10-12 14:43:10 +0200

    Don't throw exception on typechecking vanished graph location after pasting

--- libs/luna-empire/src/Empire/Commands/Graph.hs
+++ libs/luna-empire/src/Empire/Commands/Graph.hs
@@ -1332,8 +1332,9 @@ rangeToMarked code range' = (start, end)
         (start, end) = Code.viewDeltasToRealBeforeMarker code span
 
 pasteText :: GraphLocation -> [Range] -> [Text] -> Empire Text
-pasteText loc@(GraphLocation file _) ranges (Text.concat -> text) = do
-    res <- withUnit (GraphLocation file (Breadcrumb [])) $ do
+pasteText loc ranges (Text.concat -> text) = do
+    let topLoc = GraphLocation.top loc
+    res <- withUnit topLoc $ do
         runASTOp $ forM (Safe.headMay ranges) $ \range -> do
             code <- use Graph.code
             let (start, end)   = rangeToMarked code range
@@ -1344,10 +1345,15 @@ pasteText loc@(GraphLocation file _) ranges (Text.concat -> text) = do
             return (code', cursorPos)
     case res of
         Just (newCode, cursorPos) -> do
-            reloadCode (GraphLocation file (Breadcrumb [])) newCode `catch` \(e::ASTParse.SomeParserException) ->
-                withUnit (GraphLocation file (Breadcrumb [])) (Graph.userState . Graph.code .= newCode >> Graph.userState . Graph.clsParseError ?= toException e)
-            typecheck loc
-            resendCodeWithCursor (GraphLocation file (Breadcrumb [])) (Just cursorPos)
+            reloadCode topLoc newCode `catch`
+                \(e::ASTParse.SomeParserException) -> do
+                    withUnit topLoc $ do
+                        Graph.userState . Graph.code .= newCode
+                        Graph.userState . Graph.clsParseError ?= toException e
+            typecheck loc `catch` \(e::BH.BreadcrumbDoesNotExistException) ->
+                -- if after reloading, our loc no longer exists, ignore error
+                return ()
+            resendCodeWithCursor topLoc (Just cursorPos)
             return newCode
         _ -> return ""
 
