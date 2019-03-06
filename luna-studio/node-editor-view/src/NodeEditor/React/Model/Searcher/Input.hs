9eef1679ae8ae9504bff5d9954019499b47d93c3
Marcin Kostrzewa <marcinkostrzewa@yahoo.com>
2019-03-06 17:09:08 +0100

    smarter hints based on lexer

--- luna-studio/node-editor-view/src/NodeEditor/React/Model/Searcher/Input.hs
+++ luna-studio/node-editor-view/src/NodeEditor/React/Model/Searcher/Input.hs
@@ -11,6 +11,24 @@ import Data.Convert (Convertible (convert))
 import Data.Text32  (Text32)
 
 
+-------------------------
+-- === SymbolKind === ---
+-------------------------
+
+-- === Definition === ---
+
+data SymbolKind
+    = ExpressionStart
+    | Function
+    | Argument
+    | Method
+    | Operator
+    deriving (Eq, Show, Generic)
+makePrisms ''SymbolKind
+
+instance Default SymbolKind where def = ExpressionStart
+instance NFData  SymbolKind
+
 ----------------------
 -- === Divided === ---
 ----------------------
@@ -18,19 +36,21 @@ import Data.Text32  (Text32)
 -- === Definition === ---
 
 -- | This datatype is a result of analysing the tokenized searcher input,
---   specifying relevant parts of the query.
+--   specifying relevant parts of the query and the prediction for next
+--   symbol that should be typed in.
 --   The query field is the word that is currently being searched, while
 --   prefix and suffix are the remaining bits, not relevant for current search.
 --   Full searcher input can be rebuilt as prefix <> query <> suffix.
 data Divided = Divided
-    { _prefix :: Text
-    , _query  :: Text
-    , _suffix :: Text
+    { _prefix               :: Text
+    , _query                :: Text
+    , _suffix               :: Text
+    , _nextSymbolPrediction :: SymbolKind
     } deriving (Eq, Generic, Show)
 
 makeLenses ''Divided
 
-instance Default     Divided where def = Divided mempty mempty mempty
+instance Default     Divided where def = Divided mempty mempty mempty def
 instance NFData      Divided
 instance Convertible Divided Text where
     convert d = let
@@ -70,7 +90,8 @@ fromStream input inputStream cursorPos = let
         (pref , q')    = Text.splitAt qBeg pref'
         (q'', suff)    = Text.breakOn " " suff'
         q              = q' <> q''
-        in DividedInput $ Divided pref q suff
+        nextSymbol     = predictSymbolKind inputStream cursorPos
+        in DividedInput $ Divided pref q suff nextSymbol
     in case mayQueryBegin of
         Nothing  -> RawInput input
         Just beg -> splitInput beg
@@ -108,18 +129,18 @@ findLambdaArgsAndEndOfLambdaArgs input' tokens = result where
 -- === Utils === --
 
 isQuerySymbol :: Lexer.Symbol -> Bool
-isQuerySymbol Lexer.Var      {} = True
-isQuerySymbol Lexer.Cons     {} = True
-isQuerySymbol Lexer.Wildcard {} = True
-isQuerySymbol Lexer.KwAll    {} = True
-isQuerySymbol Lexer.KwCase   {} = True
-isQuerySymbol Lexer.KwClass  {} = True
-isQuerySymbol Lexer.KwDef    {} = True
-isQuerySymbol Lexer.KwImport {} = True
-isQuerySymbol Lexer.KwOf     {} = True
-isQuerySymbol Lexer.Operator {} = True
-isQuerySymbol Lexer.Modifier {} = True
-isQuerySymbol _                   = False
+isQuerySymbol Lexer.Var      {}  = True
+isQuerySymbol Lexer.Cons     {}  = True
+isQuerySymbol Lexer.Wildcard {}  = True
+isQuerySymbol Lexer.KwAll    {}  = True
+isQuerySymbol Lexer.KwCase   {}  = True
+isQuerySymbol Lexer.KwClass  {}  = True
+isQuerySymbol Lexer.KwDef    {}  = True
+isQuerySymbol Lexer.KwImport {}  = True
+isQuerySymbol Lexer.KwOf     {}  = True
+isQuerySymbol (Lexer.Operator o) = o /= ","
+isQuerySymbol Lexer.Modifier {}  = True
+isQuerySymbol _                  = False
 {-# INLINE isQuerySymbol #-}
 
 isInString :: Lexer.Symbol -> Bool
@@ -128,6 +149,15 @@ isInString Lexer.StrEsc {}             = True
 isInString (Lexer.Quote _ Lexer.Begin) = True
 isInString _                           = False
 {-# INLINE isInString #-}
+
+breaksQuery :: Lexer.Symbol -> Bool
+breaksQuery (Lexer.Accessor   {}) = True
+breaksQuery (Lexer.Group      {}) = True
+breaksQuery (Lexer.List       {}) = True
+breaksQuery (Lexer.BlockStart {}) = True
+breaksQuery (Lexer.Quote _ Lexer.End) = True
+breaksQuery (Lexer.Operator ",")      = True
+breaksQuery _ = False
 
 recursiveFindQueryBegin :: [Lexer.Token Lexer.Symbol] -> Int -> Maybe Int
 recursiveFindQueryBegin []    _         = Nothing
@@ -142,8 +172,37 @@ recursiveFindQueryBegin (h:t) cursorPos = let
     maySuffixResult   = recursiveFindQueryBegin t cursorPosInSuffix
     skipWord          = appendTokenLength <$> maySuffixResult
     notInString       = not $ isInString hElement
+    queryBreak        = breaksQuery hElement
     isHQuerySymbol    = isQuerySymbol hElement
     in if cursorPos > tokenLength                       then skipWord
         else if cursorPos <= hSpanInt && isHQuerySymbol then Just 0
         else if cursorPos >  hSpanInt && notInString    then Just cursorPos
+        else if cursorPos == hSpanInt && queryBreak     then Just cursorPos
         else Nothing
+
+predictSymbolKind :: [Lexer.Token Lexer.Symbol] -> Int -> SymbolKind
+predictSymbolKind toks cursor = go ExpressionStart toks cursor where
+    go k []     _   = k
+    go k (s:ss) pos = let
+        tokSpan   = fromIntegral $ s ^. Lexer.span
+        tokOffset = fromIntegral $ s ^. Lexer.offset
+        tokLen    = tokSpan + tokOffset
+        symbol    = s ^. Lexer.element
+        in if tokSpan > pos || (tokSpan == pos && not (breaksQuery symbol))
+            then k
+            else go (next k symbol) ss (pos - tokLen)
+
+    next _ (Lexer.Var        {})     = Argument
+    next _ (Lexer.Operator   {})     = Argument
+    next _ (Lexer.Cons       {})     = Argument
+    next _ (Lexer.Accessor   {})     = Method
+    next _ (Lexer.BlockStart {})     = Function
+    next _ (Lexer.Group Lexer.Begin) = Function
+    next _ (Lexer.Group Lexer.End)   = Argument
+    next _ (Lexer.List  Lexer.Begin) = Argument
+    next Function        (Lexer.Number {}) = Operator
+    next ExpressionStart (Lexer.Number {}) = Operator
+    next Function        (Lexer.Quote _ Lexer.End) = Operator
+    next ExpressionStart (Lexer.Quote _ Lexer.End) = Operator
+    next k _ = k
+
