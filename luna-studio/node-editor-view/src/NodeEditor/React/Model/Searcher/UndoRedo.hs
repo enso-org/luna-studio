db77688a12d8efb69ee2fc33eee97373ce1b73b3
Marcin Kostrzewa <marcinkostrzewa@yahoo.com>
2019-03-20 17:43:20 +0100

    Ara's feedback

--- luna-studio/node-editor-view/src/NodeEditor/React/Model/Searcher/UndoRedo.hs
+++ luna-studio/node-editor-view/src/NodeEditor/React/Model/Searcher/UndoRedo.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE Strict #-}
+
 module NodeEditor.React.Model.Searcher.UndoRedo where
 
 import Common.Prelude
@@ -9,15 +11,15 @@ import Common.Prelude
 -- === Definition === --
 
 data InputState = InputState
-    { _text :: Text
+    { _text           :: Text
     , _selectionStart :: Int
-    , _selectionEnd :: Int
+    , _selectionEnd   :: Int
     } deriving (Show, Eq, Generic)
 
 makeLenses ''InputState
 
 instance NFData  InputState
-instance Default InputState where def = InputState "" 0 0
+instance Default InputState where def = InputState mempty 0 0
 
 ---------------------------
 -- === UndoRedoState === --
@@ -43,19 +45,19 @@ mk = UndoRedoState def def
 
 undo :: UndoRedoState -> UndoRedoState
 undo st = case st ^. undoStack of
-    []         -> st
-    inp : inps -> st & undoStack .~ inps
-                     & redoStack %~ (inp :)
+    []             -> st
+    input : inputs -> st & undoStack .~ inputs
+                         & redoStack %~ (input :)
 
 redo :: UndoRedoState -> UndoRedoState
 redo st = case st ^. redoStack of
     []         -> st
-    inp : inps -> st & undoStack %~ (inp :)
-                     & redoStack .~ inps
+    input : inputs -> st & undoStack %~ (input :)
+                         & redoStack .~ inputs
 
 setInput :: InputState -> UndoRedoState -> UndoRedoState
-setInput inp urSt = urSt & undoStack %~ (inp :)
-                         & redoStack .~ []
+setInput input urSt = urSt & undoStack %~ (input :)
+                           & redoStack .~ []
 
 setSelection :: Int -> Int -> UndoRedoState -> UndoRedoState
 setSelection start end = (set (currentInput . selectionStart) start)
@@ -66,9 +68,9 @@ setSelection start end = (set (currentInput . selectionStart) start)
 currentInput :: Lens' UndoRedoState InputState
 currentInput = lens getter setter where
     getter ur = case ur ^. undoStack of
-        []        -> ur ^. initial
-        (inp : _) -> inp
-    setter ur inp = case ur ^. undoStack of
-        []      -> ur & initial .~ inp
-        (_ : _) -> ur & undoStack . ix 0 .~ inp
+        []          -> ur ^. initial
+        (input : _) -> input
+    setter ur input = case ur ^. undoStack of
+        []      -> ur & initial .~ input
+        (_ : _) -> ur & undoStack . ix 0 .~ input
 
