2e4368209398f66c8a37b809d68fc32c224d0d35
Marcin Kostrzewa <marcinkostrzewa@yahoo.com>
2019-03-22 18:02:37 +0100

    WIP

--- libs/luna-studio-common/src/LunaStudio/Data/Searcher/Hint.hs
+++ libs/luna-studio-common/src/LunaStudio/Data/Searcher/Hint.hs
@@ -3,8 +3,10 @@ module LunaStudio.Data.Searcher.Hint where
 
 import Prologue
 
+import qualified Data.Aeson as Aeson
+
 import Control.Lens (Getter, to)
-import Data.Aeson   (ToJSON)
+import Data.Aeson   (ToJSON, FromJSON)
 import Data.Binary  (Binary)
 import Data.Text    (Text)
 
@@ -22,6 +24,10 @@ data Raw = Raw
 
 makeLenses ''Raw
 
-instance Binary Raw
-instance NFData Raw
-instance ToJSON Raw
+instance Binary   Raw
+instance NFData   Raw
+instance ToJSON   Raw
+instance FromJSON Raw where
+    parseJSON = Aeson.withObject "Raw" $ \v ->
+        Raw <$> v Aeson..: "name"
+            <*> v Aeson..: "documentation"
